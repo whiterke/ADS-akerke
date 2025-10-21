42dffcb701c61ab4cebbe4a0e54a72e99bb8b3d9
whiterke <a_yerkinbek@kbtu.kz>
2025-10-21 09:40:26 +0500

    Add files via upload

---
+++ lab5/TaskA (1).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    priority_queue<long long,vector<long long>,greater<long long>>pq;
+    
+    long long n, x, sum = 0;
+    
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++){
+        cin >> x;
+        pq.push(x);
+    }
+    
+    //3 5 6 9
+    
+    while(pq.size() != 1){
+        long long u = pq.top();
+        pq.pop();
+        long long v = pq.top();
+        pq.pop();
+        pq.push(u + v);
+        sum += u + v;
+    }
+    cout << sum;
+    
+    return 0;
+}
---
+++ lab5/TaskE.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    priority_queue<long long,vector<long long>,greater<long long>>pq;
+    
+    long long n, m, x, sum = 0;
+    string s;
+    
+    cin >> n >> m;
+    
+    for(int i = 1; i <= n; i++){
+        cin >> s;
+        if(s == "insert"){
+            cin >> x;
+            if(pq.size() == m){
+                long long u = pq.top();
+                sum -= u;
+                pq.pop();
+                pq.push(max(u, x));
+                sum += max(u, x);
+            } else {
+                pq.push(x);
+                sum += x;
+            }
+        }
+        else {
+            cout << sum << '\n';
+        }
+    }
+    
+    return 0;
+}
